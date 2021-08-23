f4ff2177881912988329e4e999eaabdaef13895c
Daniel Chin <daniel_chin@yahoo.com>
2021-08-23 16:07:29 +0800

    prevent duplicate renaming

--- frames2Video.py
+++ frames2Video.py
@@ -20,12 +20,18 @@ def main():
     assert 'y' == input(
         f'There are {n} files here. Proceed? y/n > '
     ).lower()
-    with Jdt(n) as j:
-        for i, fn in enumerate(files):
-            _, ext = splitext(fn)
-            os.rename(fn, f'{i}{ext}')
-            j.acc()
-    print('Rename complete. ')
+    dest_names = []
+    for i, fn in enumerate(files):
+        _, ext = splitext(fn)
+        dest_names.append(f'{i}{ext}')
+    if set(files).intersection(dest_names):
+        print('Already did rename. Skipping. ')
+    else:
+        with Jdt(n) as j:
+            for fn, dest in zip(files, dest_names):
+                os.rename(fn, dest)
+                j.acc()
+        print('Rename complete. ')
     os.chdir(folder)
     base = basename(folder)
     command = f'{FFMPEG} -r 30 -i %d.jpg ../{base}.mp4'
